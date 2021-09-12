b6a6c0501eb5a29c6ad247efd2d58d181042994b
AnaComBia <anabeatrizetorres@gmail.com>
2021-09-11 21:33:46 -0300

    finaliza programa Metros, Km e Cm

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MediasKmMCm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nfale a médida em Metros: ");
+            double MedidaMetros = Convert.ToDouble(Console.ReadLine());
+
+            double MedidaKm = MedidaMetros / 1000;
+            double MedidaCm = MedidaMetros * 100;
+
+            Console.Write($"\n----Resultado---\n Km = {MedidaKm}\n M = {MedidaMetros}\n Cm = {MedidaCm}\n");
+
+
+    
+        }
+    }
+}
